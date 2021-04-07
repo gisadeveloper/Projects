fcf24c4d219892bd858d9ad8a9919162fb83608e
gisadev <gisadev@outlook.com>
2021-04-07 14:57:56 -0400

    RickandMortyApp

---
+++ Angular/02-RickandMortyGraphQLApp/src/app/shared/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const QUERY = gql`
+
+`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
